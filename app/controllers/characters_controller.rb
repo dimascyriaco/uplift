72aaff7b6dfec4606ec733ebaffbeeba83c2ae58
Dimas Cyriaco <dimascyriaco@gmail.com>
2014-01-25 22:56:35 -0200

    Fixing character update

--- app/controllers/characters_controller.rb
+++ app/controllers/characters_controller.rb
@@ -1,6 +1,7 @@
 class CharactersController < ApplicationController
   def show
     @character = Character.find(params[:id])
+    @gurps_character = @character.gurps_character
   end
 
   def new
@@ -12,7 +13,13 @@ class CharactersController < ApplicationController
     redirect_to character_path(character)
   end
 
-private
+  def update
+    character = Character.find(params[:id])
+    character.update_attributes! character_params
+    redirect_to character_path(character)
+  end
+
+  private
 
   def character_params
     params.require(:character).permit(:name, :st, :dx, :iq, :ht)
